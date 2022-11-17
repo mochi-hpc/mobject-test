a5b5b6bf235c18e09a68dea1dc05f54b1422ba8f
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-11-17 15:05:21 -0600

    ci: remove kill

--- ci.sh
+++ ci.sh
@@ -30,4 +30,4 @@ make check
 export MOBJECT_CLUSTER_FILE=$PWD/mobject.ssg
 bedrock na+sm -c $PWD/tests/config.json -v trace &
 ior -g -a RADOS -t 64k -b 128k --rados.user=foo --rados.pool=bar --rados.conf $MOBJECT_CLUSTER_FILE
-kill %1
+
